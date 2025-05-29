c3d9c6ce78388639932e51ad095cb573d7e02b1b
Mario Bueno <maarioo2525@gmail.com>
2025-05-30 01:12:13 +0200

    aawd

--- server.js
+++ server.js
@@ -209,7 +209,7 @@ passport.use('spotify-link', new SpotifyStrategy({
   app.use('/amistades', require('./src/routes/amistadesRoutes'));
   app.use('/conversaciones', require('./src/routes/conversacionesRoutes'));
   app.use('/notificaciones', require('./src/routes/notificacionesRoutes'));
-  app.USE('/spotify', require('./src/routes/spotifyRoutes'))
+  app.use('/spotify', require('./src/routes/spotifyRoutes'))
 
   // Conexión a Mongo
   mongoose.connect(process.env.MONGO_URL)
