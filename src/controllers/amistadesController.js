ad58850818872d0fa4d605a4a5662d645a93cebc
Mario Bueno <maarioo2525@gmail.com>
2025-05-29 16:00:01 +0200

    Nose que he cambiado

--- src/controllers/amistadesController.js
+++ src/controllers/amistadesController.js
@@ -1,12 +1,39 @@
 const Amistad = require('../models/amistad');
 const SolicitudAmistad = require('../models/solicitudAmistad');
+const Usuario = require('../models/usuario');
 
 exports.obtenerAmistades = async (req, res) => {
-  const amistades = await Amistad.find({
-    $or: [{ usuario_id_1: req.params.usuarioId }, { usuario_id_2: req.params.usuarioId }],
-    estado: 'aceptada',
+  const userId = req.params.usuarioId;
+  // buscamos las amistades aceptadas e incluimos info de ambos usuarios
+  const amistades = await Amistad
+    .find({
+      $or: [
+        { usuario_id_1: userId },
+        { usuario_id_2: userId }
+      ],
+      estado: 'aceptada'
+    })
+    .populate('usuario_id_1', 'nombre foto_perfil ubicarion playlistsPublicas estado cancionDestacada ultimaActividad online')
+    .populate('usuario_id_2', 'nombre foto_perfil ubicarion playlistsPublicas estado cancionDestacada ultimaActividad online');
+
+  // transformamos para devolver siempre el otro usuario como “amigo”
+  const friends = amistades.map(a => {
+    const amigo = a.usuario_id_1._id.toString() === userId
+      ? a.usuario_id_2
+      : a.usuario_id_1;
+    return {
+      id: amigo._id,
+      nombre: amigo.nombre,
+      foto_perfil: amigo.foto_perfil,
+      online: amigo.online,
+      playlistsPublicas: amigo.playlistsPublicas,
+      estado: amigo.estado,
+      cancionDestacada: amigo.cancionDestacada,
+      ultimaActividad: amigo.ultimaActividad
+    };
   });
-  res.json(amistades);
+
+  res.json(friends);
 };
 
 exports.enviarSolicitudAmistad = async (req, res) => {
