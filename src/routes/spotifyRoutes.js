43c8bc59159f06af38e4a8eb9d116ff5b99ff93d
Mario Bueno <maarioo2525@gmail.com>
2025-05-31 01:43:50 +0200

    Small fix

--- src/routes/spotifyRoutes.js
+++ src/routes/spotifyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerPlaylistsDeSpotify } = require('../controllers/spotifyController');
+const { obtenerPlaylistsDeSpotify, obtenerRecomendacionesDeSpotify } = require('../controllers/spotifyController');
 
 /**
  * @swagger
