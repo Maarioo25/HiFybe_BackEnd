7357d632d27fdc69ee6c0490b811486630b871a6
Mario Bueno <maarioo2525@gmail.com>
2025-06-01 03:06:07 +0200

    Small fix

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ passport.use(new SpotifyStrategy({
   clientID: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   callbackURL: process.env.SPOTIFY_CALLBACK_URL,
-  passReqToCallback: true,
-  skipUserProfile: true
+  passReqToCallback: true
 }, async (req, accessToken, refreshToken, profile, done) => {
   try {
     const User = require('./src/models/usuario');
