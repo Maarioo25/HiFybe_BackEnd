945e3d970fff0e7b6b8fb4ae281fa5975285a590
Mario Bueno <maarioo2525@gmail.com>
2025-06-03 15:46:48 +0200

    Imagen en los mensajes

--- src/models/mensaje.js
+++ src/models/mensaje.js
@@ -5,7 +5,7 @@ const mensajeSchema = new mongoose.Schema({
     emisor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
     contenido: String,
     fecha_envio: { type: Date, default: Date.now },
-    cancion: { uri: { type: String }, titulo: { type: String }, artista: { type: String }},
+    cancion: { uri: { type: String }, titulo: { type: String }, artista: { type: String }, imagen: {type: String}},
     leido: { type: Boolean, default: false },
   }, { collection: 'mensajes' });
   
