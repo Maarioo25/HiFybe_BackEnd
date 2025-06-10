157042bcc930e4e91b50ca2ae8ed97eee94dd73d
Maarioo25 <maarioo2525@gmail.com>
2025-06-11 01:21:58 +0200

    callbackURL

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ const fetch = require('node-fetch');
 passport.use(new SpotifyStrategy({
   clientID: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  callbackURL: process.env.SPOTIFY_CALLBACK_URL,
   passReqToCallback: true,
   skipUserProfile: true
 }, async (req, accessToken, refreshToken, profile, done) => {
